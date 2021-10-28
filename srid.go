38c3eab2088287cb94e59d41c674118af8bb03fd
aninggo <aninggo@gmail.com>
2021-10-28 09:27:49 +0800

    update

--- srid.go
+++ srid.go
@@ -1,6 +1,10 @@
 package geom
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 func SridToUrn(srid int) string {
 	if int(srid) == 4326 {
@@ -8,4 +12,21 @@ func SridToUrn(srid int) string {
 	}
 
 	return fmt.Sprintf("urn:ogc:def:crs:EPSG::%d", srid)
+}
+
+func UrnToSrid(urn string) int {
+	if urn == "urn:ogc:def:crs:OGC:1.3:CRS84" {
+		return 4326
+	}
+
+	if strings.HasPrefix(urn, "urn:ogc:def:crs:EPSG::") {
+		estr := strings.TrimPrefix(urn, "urn:ogc:def:crs:EPSG::")
+		epsg, err := strconv.Atoi(estr)
+
+		if err == nil {
+			return epsg
+		}
+	}
+
+	return -1
 }
